9ac72afb58e5e9ea417082824c01f62439df6018
hyeonji1125 <{guswl665@naver.com}>
2024-04-11 17:19:34 +0900

    example

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+import Example from './pages/Example'
+import Example2 from './pages/Example2'
+import Example3 from './pages/Example3'
+import Example4 from './pages/Example4'
+import Example5 from './pages/Example5'
+
+
+function App() {
+
+  return (
+    <>
+    <Example />
+    <Example2 />
+    <Example3 />
+    <Example4 />
+    <Example5 />
+    </>
+
+      
+  )
+}
+
+export default App
